16c29e08783ecefab266ebc336b0e913e7131098
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2014-06-09 17:24:42 +0200

    First commit of Transient_Cloud_Client application. Basic watcher implemented.

---
+++ Transient_Cloud_Client/Watcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Transient_Cloud_Client
+{
+    class Watcher
+    {
+        private static FileSystemWatcher watcher = new FileSystemWatcher();
+        public String Path { get; set; }
+        private static String extensionsToWatch = "*.*";
+        static Watcher()
+        {
+            watcher.Filter = extensionsToWatch;
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.IncludeSubdirectories = true;
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+        }
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter directory to watch");
+            watcher.Path = Console.ReadLine();
+
+            // Start raising events
+            watcher.EnableRaisingEvents = true;
+            while (Console.Read() != 'q') ;
+        }
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+        }
+    }
+}
